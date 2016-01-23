f29dbf567e0b1d2326e44f356733ac9fe1a0dec1
JonZeolla <Zeolla@gmail.com>
2016-01-23 16:26:22 -0500

    Delete Ubuntu14_Install.sh

--- Software-Defined-Radios_2016-02-11/setup/Ubuntu14_Install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Tested on Ubuntu 14.04 as of 2016-01-23
-
-sudo apt-get install git python-scipy swig libboost-all-dev qtdeclarative5-dev libqt5svg5-dev
-
-git clone --recursive https://github.com/pybombs/pybombs.git
-cd pybombs
-./pybombs install gqrx
