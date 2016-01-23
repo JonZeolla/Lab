9ef9584c7911e1e4f84b5bbbc250eda39dfe3d35
JonZeolla <Zeolla@gmail.com>
2016-01-23 15:50:08 -0500

    Update Ubuntu14_Install.sh

--- Software-Defined-Radios_2016-02-11/setup/Ubuntu14_Install.sh
+++ Software-Defined-Radios_2016-02-11/setup/Ubuntu14_Install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Tested on Ubuntu 14.04 as of 2016-01-23
 
-sudo apt-get install git python-scipy swig libboost-all-dev qtdeclarative5-dev
+sudo apt-get install git python-scipy swig libboost-all-dev qtdeclarative5-dev libqt5corea
 
 git clone --recursive https://github.com/pybombs/pybombs.git
 cd pybombs
