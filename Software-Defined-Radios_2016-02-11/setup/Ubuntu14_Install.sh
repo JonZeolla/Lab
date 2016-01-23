c09c9f06a3390d62b181ac45dd0c41ca36555b83
JonZeolla <Zeolla@gmail.com>
2016-01-23 13:34:02 -0500

    Update Ubuntu14_Install.sh

--- Software-Defined-Radios_2016-02-11/setup/Ubuntu14_Install.sh
+++ Software-Defined-Radios_2016-02-11/setup/Ubuntu14_Install.sh
@@ -5,6 +5,4 @@ sudo apt-get install git python-scipy swig libboost-all-dev
 
 git clone --recursive https://github.com/pybombs/pybombs.git
 cd pybombs
-./pybombs install gqrx-digital
 ./pybombs install gqrx
-./pybombs env
