dd3b3ff1a6d47a4cf09e0c8244fb74676f0379f6
JonZeolla <Zeolla@gmail.com>
2016-02-09 19:42:53 -0500

    Update TuneIn.sh

--- Labs/FM-Radio/TuneIn.sh
+++ Labs/FM-Radio/TuneIn.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-gqrx
+shopt -s nocasematch
+if [ $# -eq 0 ]; then
+  echo -e "Usage: $0 (TuneIn|Recording)"
+  exit 1
+fi
+
+if [[ (${1} == "TuneIn") || (${1} == "Tune" && ${2} == "In") ]]; then
+  gqrx
+elif [[ ${1} == "Recording" ]]
+  aplay -r 48k -f S16_LE -t raw -c 1 ${HOME}/Desktop/Lab/Labs/FM-Radio/gqrx_20160210_001957_105900000.wav
+else
+  echo -e "Usage: $0 (TuneIn|Recording)"
+fi
+shopt -u nocasematch
